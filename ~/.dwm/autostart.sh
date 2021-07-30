4bb7d1c8b2e76e51a5b7a339b748eea4b5904c8d
mstefk <68973887+mstefk@users.noreply.github.com>
2021-07-31 01:46:24 +0200

    put executable permission to it. "chmod +x ~/.dwm/autostart.sh"
    put executable permission to it. "chmod +x ~/.dwm/autostart.sh"

--- ~/.dwm/autostart.sh
+++ ~/.dwm/autostart.sh
@@ -1,6 +1,5 @@
 exec setxkbmap -layout rs,rs -variant latin,  &
 xrdb /home/archuser/.Xresources &
-#exec dwm &
 exec feh --bg-fill /home/archuser/Pictures/wallpaper &
 exec /home/archuser/Downloads/dwm-bar/dwm_bar.sh 
 #STR= $(/home/archuser/Downloads/xkb-switch/build/xkb-switch -W) &
