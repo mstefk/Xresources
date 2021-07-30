a0cb7b86342a056314a650a88d6da3567a6de1cf
mstefk <68973887+mstefk@users.noreply.github.com>
2021-07-31 01:43:54 +0200

    Create autostart.sh

---
+++ ~/.dwm/autostart.sh
@@ -0,0 +1,11 @@
+exec setxkbmap -layout rs,rs -variant latin,  &
+xrdb /home/archuser/.Xresources &
+#exec dwm &
+exec feh --bg-fill /home/archuser/Pictures/wallpaper &
+exec /home/archuser/Downloads/dwm-bar/dwm_bar.sh 
+#STR= $(/home/archuser/Downloads/xkb-switch/build/xkb-switch -W) &
+#while true; do
+#  xsetroot -name "$(date +'%b/%d/%Y %H:%M')" &
+#  #xsetroot -name "$(/home/archuser/Downloads/xkb-switch/build/xkb-switch -W)" &
+#  sleep 15
+#done
