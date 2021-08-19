1079215a95109f7ecd3b94f58e39f2da75f56404
mstefk <68973887+mstefk@users.noreply.github.com>
2021-08-20 00:43:28 +0200

    Create verysimplepasswordmanager.sh

---
+++ verysimplepasswordmanager.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+#TO ADD : vspm add <name of file you want the password to be stored> <password>
+#TO GET PASSWORD : vpsm add <name of password>
+#gpg --decrypt SECURE.gpg 2>/dev/null | xclip -selection c
+if [[ $1 == "add" && -n $2 && -n $3 ]];then
+echo $3 | gpg --output ~/.pass_store/$2 --symmetric
+elif [[ $1 == "get" && -n $2 ]];then
+gpg --decrypt ~/.pass_store/$2 2>/dev/null | xclip -selection c
+fi
